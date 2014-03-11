6246df358a1a7e58301b61ad0e2b7eb5dead6440
Jay Hotta <j-hotta@creationline.com>
2014-03-11 17:31:17 +0900

    add a new document

--- python/input-test2.py
+++ python/input-test2.py
@@ -6,8 +6,10 @@ data = []
 
 while 1:
 	for charcter in file.read(1):
-		print charcter
+		# print charcter
+		print ord(charcter)
 		data += ['%02X' % ord(charcter)]
+		print data
 		if len(data) == 8:
 			for byte in data:
 				sys.stdout.write(byte+' ')
