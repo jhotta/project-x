f270f94b7bf64ca00266af98facb7a1daa886007
Jay Hotta <j-hotta@creationline.com>
2014-03-11 13:31:42 +0900

    revise toy scripts

---
+++ python/input-test2.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+import sys
+file = open('/dev/input/js0','r')
+data = []
+
+while 1:
+	for charcter in file.read(1):
+		print charcter
+		data += ['%02X' % ord(charcter)]
+		if len(data) == 8:
+			for byte in data:
+				sys.stdout.write(byte+' ')
+			sys.stdout.write('\n')
+			sys.stdout.flush()
+			data = []	
