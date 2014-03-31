bf7bdf6ae459d4188c0a46883fd75555b3838127
Jay Hotta <j-hotta@creationline.com>
2014-03-31 15:01:54 +0900

    add a new dir

--- python/pad2drone_controler/hori2droneAPI.py
+++ python/pad2drone_controler/hori2droneAPI.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 # import pdb; pdb.set_trace()
 
-import pygame
 import sys
+import pygame
+import pygame.surfarray
+import pygame.transform
+import libardrone
 
 def init_gamepad():
   pygame.joystick.init()
@@ -14,9 +17,26 @@ def init_gamepad():
   except pygame.error:
     print "Unexpected error:", sys.exc_info()[0]
 
-def get_gamepad_action(pad):
+def init_ardrone():
+  try:
+    drone = libardrone.ARDrone(True)
+    drone.reset()
+    return drone
+  except:
+    print "Unexpected error:", sys.exc_info()[0]
+
+def full_hatl(drone):
+  print "Shutting down...",
+  drone.halt()
+  print "Ok."
+
+def get_gamepad_action(pad, drone):
+
+  W, H = 320, 240
+  # clock = pygame.time.Clock()
+  # running = True
+
   axis_value = {'0':0.0, '1':0.0, '2':0.0, '3':0.0}
-  pygame.init()
   while 1:
     for e in pygame.event.get():
       if e.type == pygame.JOYAXISMOTION: # 7
@@ -30,7 +50,12 @@ def get_gamepad_action(pad):
       elif e.type == pygame.JOYBUTTONUP: # 11
         print "%s button released"% str(e.button)
 
+def main():
+  pad = init_gamepad()
+  drone = init_ardrone()
+  pygame.init()
+  get_gamepad_action(pad, drone)
+  full_hatl(drone)
 
 if __name__ == "__main__":
-  pad = init_gamepad()
-  get_gamepad_action(pad)
+  main()
